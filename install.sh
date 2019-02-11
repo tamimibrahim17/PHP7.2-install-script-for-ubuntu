02b9443e7e6a089ced6c4a3b3cf2bfa28da11d9b
Tamim Ibrahim <tamimibrahim17@gmail.com>
2019-02-11 19:11:20 +0600

    gmp installation

--- install.sh
+++ install.sh
@@ -21,4 +21,4 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 
 # install php7.2 and it's essentail extention
-sudo apt-get install -y php7.2-fpm php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-recode php7.2-tidy php7.2-dev php7.2-intl php7.2-snmp php7.2-curl php7.2-phpdbg php7.2-mbstring php7.2-zip php7.2-soap php7.2-xml php7.2-sqlite3 php7.2-gd
+sudo apt-get install -y php7.2-fpm php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-recode php7.2-tidy php7.2-dev php7.2-intl php7.2-snmp php7.2-curl php7.2-phpdbg php7.2-mbstring php7.2-zip php7.2-soap php7.2-xml php7.2-sqlite3 php7.2-gd php7.2-gmp
